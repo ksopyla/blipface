96c9e72916330423e05c5f750e38e9225c2279b9
ksirg <ksirg@wp.pl>
2009-03-19 14:45:25 +0100

    Dodanie projektu, na razie nic specjalengo nie robi, podpowiada tylko ilość wpisywanych liter w okientku

---
+++ BlipFace/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlipFace
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        const int BlipSize = 120;
+        int charLeft=BlipSize;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbMessage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            charLeft = BlipSize - tbMessage.Text.Length;
+            if(charLeft<0)
+            {
+                tblCharLeft.Foreground = new SolidColorBrush(Color.FromRgb(200,100,100));
+                btnSendBlip.IsEnabled = false;
+            }else {
+                tblCharLeft.Foreground = new SolidColorBrush(Color.FromRgb(160,160,160));
+                btnSendBlip.IsEnabled = true;
+            }
+
+            tblCharLeft.Text = charLeft.ToString();
+        }
+    }
+}
