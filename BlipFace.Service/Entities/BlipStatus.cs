e54d584092fdb1b0e40b6c7d01fafb970cfcff68
ksirg <ksirg@wp.pl>
2009-03-20 18:45:50 +0100

    Dodanie nowego projetku BlipFace.Service służącego jako warstwa logiki, przeniesienie klasy BlipStatus do nowego projektu Dodanie pliku Enterprise Architect'a z podstawowymi diagramami

--- BlipFace.Service/Entities/BlipStatus.cs
+++ BlipFace.Service/Entities/BlipStatus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BlipFace
+namespace BlipFace.Service.Entities
 {
     public class BlipStatus
     {
---
+++ BlipFace/View/IMainView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlipFace.Service.Entities;
+
+namespace BlipFace.View
+{
+
+    /// <summary>
+    /// Interfejs głównego okna,
+    /// Definiuje podstawową odpowiedzialność naszego widoku, jakie dane powienien 
+    /// przechowywać, oraz jakie operacje powinien wystawiać
+    /// 
+    /// </summary>
+    public interface IMainView
+    {
+        /// <summary>
+        /// Lista wszystkich statusów użytkowników w onie
+        /// </summary>
+        IList<BlipStatus> Statuses { get; set; }
+
+        /// <summary>
+        /// Główny(ostatni) status użytkownika
+        /// </summary>
+        BlipStatus MainStatus { get; set; }
+
+
+    }
+}
