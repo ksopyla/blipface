2cf86a1f8da030456f4f3baedab48c7b21c71433
ksirg <ksirg@wp.pl>
2009-03-20 19:29:42 +0100

    Dodanie klasy do obsługi komunikacji z blipem, dodanie prezentera do widoku

---
+++ BlipFace.Service/Communication/BlipCommunication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Http;
+using System.Runtime.Serialization.Json;
+using System.Net;
+using BlipFace.Service.Entities;
+
+namespace BlipFace.Service.Communication
+{
+
+    /// <summary>
+    /// Główna klasa służąca do komunikacji z api blip.pl
+    /// 
+    /// Na razie zastanawiamy się czy to nie będzie singleton,
+    /// a może same metody statyczne?
+    /// </summary>
+    public class BlipCommunication
+    {
+        /// <summary>
+        /// Klasa z WCF Rest Starter Kit (http://msdn.microsoft.com/netframework/cc950529(en-us).aspx)
+        /// </summary>
+        private HttpClient blipHttpClient = new HttpClient("http://api.blip.pl");
+
+
+        private string userName;
+        private string password;
+
+
+        public BlipCommunication(string _userName,string _password)
+        {
+
+            userName = _userName;
+            password = _password;
+
+            blipHttpClient.TransportSettings.Credentials = new NetworkCredential(userName,password);
+        }
+
+
+        public IList<BlipStatus> GetAllStatuses(int limit,bool withDirectMessage)
+        {
+
+            HttpResponseMessage resp=  blipHttpClient.Get("updates");
+                //, new HttpQueryString().Add("limit", limit.ToString()));
+
+            //resp.Content.ReadAsJsonDataContract
+
+            return null;
+        }
+    }
+}
---
+++ BlipFace/Presenter/MainPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlipFace.Presenter
+{
+
+    /// <summary>
+    /// Klasa prezentera do naszego głównego widoku, zgodnie z wzorcem MVP
+    /// </summary>
+    internal class MainPresenter
+    {
+
+        public void AddStatus()
+        {
+        }
+
+        public void GetStatuses()
+        {
+        }
+    }
+}
