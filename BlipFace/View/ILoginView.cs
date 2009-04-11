032535ad0b17b9c224f69f00d17173d900259658
ksirg <ksirg@wp.pl>
2009-04-11 20:57:11 +0200

    Porządki z stylami do aplikacji, dodanie interfejsu widoku z logowaniem

---
+++ BlipFace/View/ILoginView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlipFace.View
+{
+    public interface ILoginView
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
