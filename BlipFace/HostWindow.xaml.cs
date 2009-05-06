812933cc5f7ea2ba4a782b54f1fa2747401df338
ksirg <ksirg@wp.pl>
2009-05-06 17:51:48 +0200

    Dodanie obsługi minimalizowania aplikacji, oraz dodano do przycsików w prawym górnym rogu delikatne podświetlenia po najechaniu

--- BlipFace/HostWindow.xaml.cs
+++ BlipFace/HostWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,21 +21,46 @@ namespace BlipFace
     public partial class HostWindow : Window, IHostView
     {
         private ViewsManager mgr;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
 
 
         public HostWindow()
         {
             InitializeComponent();
+            
+            //położenie okna 
             this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width - 20;
+            
+            
+            //ikona dla aplikacji, pozakuje się na pasku
+            //blipFace_logo_round.png"
+            Uri iconUri = new Uri("pack://application:,,,/Resource/Img/blipFace.ico",
+                                  UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+
+
+            //ikona w sys tray'u po zminimalizowaniu
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.BalloonTipText = "BlipFace się narazie howa, jeżeli chcesz go zobaczyć jeszcze raz kliknij na ikonę.";
+            notifyIcon.BalloonTipTitle = "BlipFace";
+            notifyIcon.Text = "Kliknij aby pokazał się BlipFace";
+
+            Stream iconStream = Application.GetResourceStream(iconUri).Stream;
+            notifyIcon.Icon = new System.Drawing.Icon(iconStream);
+            
+            
+            notifyIcon.Click += new EventHandler(NotifyIconClick);
+
+
             mgr = new ViewsManager(this);
         }
+
+        
 
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Uri iconUri = new Uri("pack://application:,,,/Resource/Img/blipFace_logo_round.png",
-                                  UriKind.RelativeOrAbsolute);
-            this.Icon = BitmapFrame.Create(iconUri);
+            
             mgr.Run();
         }
 
@@ -82,7 +109,91 @@ namespace BlipFace
 
         #endregion
 
+        private void btnMinimalizeApp_Click(object sender, RoutedEventArgs e)
+        {
+           // WindowState = System.Windows.WindowState.Minimized;
+
+            this.Hide();
+            if (notifyIcon != null)
+                notifyIcon.ShowBalloonTip(2000);
+        }
+
         #region IHost Members
+
+        #endregion
+
+
+        //implementacja howania okna zaczerpnięta z 
+        //http://possemeeg.wordpress.com/2007/09/06/minimize-to-tray-icon-in-wpf/
+        #region chowanie okna 
+
+        void NotifyIconClick(object sender, EventArgs e)
+        {
+            
+            Show();
+        //if(WindowState== System.Windows.WindowState.Maximized)
+        //{
+        //    this.WindowState = System.Windows.WindowState.Normal;
+        //}
+        //else
+             this.WindowState = storedWindowState;
+        }
+
+        /// <summary>
+        /// wywoływana przed zamknęciem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HostWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            //upewniamy się że ikona zostanie usunięta
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+
+        private WindowState storedWindowState = WindowState.Normal;
+
+        /// <summary>
+        /// Gdy zmieni się stan okna np. z normalnego do minmalizowanego
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HostWindow_OnStateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+                if (notifyIcon != null)
+                    notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+                storedWindowState = WindowState.Normal;
+                    //WindowState;
+        }
+
+        /// <summary>
+        /// Kiedy zmieni się widoczność okna
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HostWindow_OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            CheckTrayIcon();
+
+        }
+
+        private void CheckTrayIcon()
+        {
+
+            ShowTrayIcon(!IsVisible);
+        }
+
+        private void ShowTrayIcon(bool show)
+        {
+
+            if (notifyIcon != null)
+                notifyIcon.Visible = show;
+        }
 
         #endregion
     }
