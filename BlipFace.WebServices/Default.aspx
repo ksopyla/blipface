4798fdaa74fc3c399df34f57c218a44a8bcc39fc
daniel.plawgo@gmail.com <daniel.plawgo@gmail.com>
2009-08-19 14:04:34 +0200

    Dodanie strony z statystykami

---
+++ BlipFace.WebServices/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BlipFace.WebServices.Default" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35"
+    Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+<%@ Register Assembly="obout_Calendar2_Net" Namespace="OboutInc.Calendar2" TagPrefix="obout" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Statyski BlipFace</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Początek okresu:
+        <asp:TextBox ID="StartTimeTextBox" runat="server" Enabled="False" Width="80px"></asp:TextBox>
+        <obout:Calendar ID="StartCalendar" runat="server" DatePickerMode="True" TextBoxId="StartTimeTextBox"
+            TitleText="Wybierz datę" CultureName="pl-PL" DatePickerImagePath="/Img/dateselect.gif"
+            DatePickerImageTooltip="Wybierz datę" AutoPostBack="True">
+        </obout:Calendar>
+        Koniec okresu:
+        <asp:TextBox ID="EndTimeTextBox" runat="server" Enabled="False" Width="80px"></asp:TextBox>
+        <obout:Calendar ID="EndCalendar" runat="server" DatePickerMode="True" TextBoxId="EndTimeTextBox"
+            TitleText="Wybierz datę" CultureName="pl-PL" DatePickerImagePath="/Img/dateselect.gif"
+            DatePickerImageTooltip="Wybierz datę" AutoPostBack="True">
+        </obout:Calendar>
+        <br />
+        Ilość unikalnych użytkowników:<asp:Label ID="UniqUsersLabel" runat="server"></asp:Label>
+        <br />
+        <asp:Chart ID="UsersUseBlipFaceChart" runat="server" Height="500px" Width="600px">
+            <Series>
+                <asp:Series Name="Series1">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+        <br />
+        <asp:Chart ID="UsesVersionBlipFaceChart" runat="server" Height="500px" Width="600px">
+            <Series>
+                <asp:Series Name="Series1">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+    </div>
+    </form>
+</body>
+</html>
---
+++ BlipFace.WebServices/Default.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace BlipFace.WebServices
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                StartCalendar.SelectedDate = DateTime.Now.AddDays(-10);
+                StartCalendar.DataBind();
+                EndCalendar.SelectedDate = DateTime.Now;
+                EndCalendar.DataBind();
+            }
+
+            using (DataClassesDataContext db = new DataClassesDataContext())
+            {
+                #region UsersUseBlipFaceInDay
+                var UsersUseBlipFaceInDay = from usesBlipFace in db.CountUses
+                                            where usesBlipFace.DateUse.Date.CompareTo(StartCalendar.SelectedDate.Date) >= 0 && usesBlipFace.DateUse.Date.CompareTo(EndCalendar.SelectedDate.Date) <= 0
+                                            group usesBlipFace by usesBlipFace.DateUse.Date into useInDay
+                                            select new { Date = useInDay.Key, CountUse = useInDay.ToList().GroupBy(a => a.UserGuid).Count() };
+
+                // Set series chart type
+                UsersUseBlipFaceChart.Series["Series1"].ChartType = SeriesChartType.Bar;
+
+                // Set series point width
+                UsersUseBlipFaceChart.Series["Series1"]["PointWidth"] = "0.8";
+
+                // Show data points labels
+                UsersUseBlipFaceChart.Series["Series1"].IsValueShownAsLabel = true;
+
+                // Set data points label style
+                UsersUseBlipFaceChart.Series["Series1"]["BarLabelStyle"] = "Center";
+
+                // Draw as 3D Cylinder
+                UsersUseBlipFaceChart.Series["Series1"]["DrawingStyle"] = "Cylinder";
+
+                UsersUseBlipFaceChart.DataSource = UsersUseBlipFaceInDay.ToList(); ;
+                UsersUseBlipFaceChart.Series["Series1"].XValueMember = "Date";
+                UsersUseBlipFaceChart.Series["Series1"].YValueMembers = "CountUse";
+                UsersUseBlipFaceChart.Titles.Add("Ilość użytkowników BlipFace w poszczególnych dniach");
+
+                UsersUseBlipFaceChart.DataBind();
+                #endregion
+
+                #region UniqUserUsesBlipFace
+
+                var uniqUserUserBlipFace = from userGuid in db.CountUses
+                                           where userGuid.DateUse.Date.CompareTo(StartCalendar.SelectedDate.Date) >= 0 && userGuid.DateUse.Date.CompareTo(EndCalendar.SelectedDate.Date) <= 0
+                                           group userGuid by userGuid.UserGuid into uniqUser
+                                           select uniqUser;
+
+                UniqUsersLabel.Text = uniqUserUserBlipFace.Count().ToString();
+
+                #endregion
+
+                #region UsesVersionBlipFace
+                var usesVersionBlipFace = from usesBlipFace in db.CountUses
+                                            where usesBlipFace.DateUse.Date.CompareTo(StartCalendar.SelectedDate.Date) >= 0 && usesBlipFace.DateUse.Date.CompareTo(EndCalendar.SelectedDate.Date) <= 0
+                                            group usesBlipFace by usesBlipFace.Version into useVersion
+                                            select new { Date = useVersion.Key, CountUse = useVersion.ToList().GroupBy(a => a.UserGuid).Count() };
+
+                // Set series chart type
+                UsesVersionBlipFaceChart.Series["Series1"].ChartType = SeriesChartType.Pie;
+
+                // Set series point width
+                UsesVersionBlipFaceChart.Series["Series1"]["PointWidth"] = "0.8";
+
+                // Show data points labels
+                UsesVersionBlipFaceChart.Series["Series1"].IsValueShownAsLabel = true;
+
+                // Set data points label style
+                UsesVersionBlipFaceChart.Series["Series1"]["BarLabelStyle"] = "Center";
+
+                // Draw as 3D Cylinder
+                UsesVersionBlipFaceChart.Series["Series1"]["DrawingStyle"] = "Cylinder";
+
+                UsesVersionBlipFaceChart.DataSource = usesVersionBlipFace.ToList(); ;
+                UsesVersionBlipFaceChart.Series["Series1"].XValueMember = "Date";
+                UsesVersionBlipFaceChart.Series["Series1"].YValueMembers = "CountUse";
+                UsesVersionBlipFaceChart.Titles.Add("Używane wersje BlipFace");
+
+                UsersUseBlipFaceChart.DataBind();
+                #endregion
+            }
+        }
+    }
+}
