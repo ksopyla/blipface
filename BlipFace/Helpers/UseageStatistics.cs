614b93c118048ae1213784bdd578faabdb5f0fe2
ksirg <ksirg@wp.pl>
2009-08-27 12:33:28 +0200

    brzydka obsługa błedów podczas sprawdzania wersji, tak testowo abym mógł zobaczyć co się wogóle wywala, zmodyfikowanie lini w app.config tej które właśnie powodowała błąd

--- BlipFace/Helpers/UseageStatistics.cs
+++ BlipFace/Helpers/UseageStatistics.cs
@@ -21,38 +21,49 @@ namespace BlipFace.Helpers
 
         private void NotyfiInNewThread()
         {
-            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-            Guid blipFaceGuid = Guid.Empty;
-
-            using (IsolatedStorageAccess isoAccess = new IsolatedStorageAccess(FileBlipFaceGuid))
+            try
             {
-                string[] credenctial = isoAccess.ReadAll();
-
-                if (credenctial != null && credenctial.Length > 0)
-                {
-                    //odczytujemy Guid danej instancji aplikacji BlipFace
-                    var stringGuid = credenctial[0].Decrypt().ToInsecureString();
-                    blipFaceGuid = new Guid(stringGuid);
-                }
-            }
-
-            //gdy nie ma jeszcze ustawionego Guida aplikacji (np. gdy BlipFace jest uruchamiany poraz pierwszy
-            if (blipFaceGuid == Guid.Empty)
-            {
-                blipFaceGuid = Guid.NewGuid();
-
-                SecureString secureGuid = blipFaceGuid.ToString().ToSecureString();
+                Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                Guid blipFaceGuid = Guid.Empty;
 
                 using (IsolatedStorageAccess isoAccess = new IsolatedStorageAccess(FileBlipFaceGuid))
                 {
-                    string[] credenctial = new[] { secureGuid.Encrypt() };
+                    string[] credenctial = isoAccess.ReadAll();
 
-                    isoAccess.WriteStrings(credenctial);
+                    if (credenctial != null && credenctial.Length > 0)
+                    {
+                        //odczytujemy Guid danej instancji aplikacji BlipFace
+                        var stringGuid = credenctial[0].Decrypt().ToInsecureString();
+                        blipFaceGuid = new Guid(stringGuid);
+                    }
                 }
-            }
 
-            BlipFaceServicesCommunication communication = new BlipFaceServicesCommunication();
-            communication.NotifyUseBlipFace(blipFaceGuid, version.ToString());
+                //gdy nie ma jeszcze ustawionego Guida aplikacji (np. gdy BlipFace jest uruchamiany poraz pierwszy
+                if (blipFaceGuid == Guid.Empty)
+                {
+                    blipFaceGuid = Guid.NewGuid();
+
+                    SecureString secureGuid = blipFaceGuid.ToString().ToSecureString();
+
+                    using (IsolatedStorageAccess isoAccess = new IsolatedStorageAccess(FileBlipFaceGuid))
+                    {
+                        string[] credenctial = new[] {secureGuid.Encrypt()};
+
+                        isoAccess.WriteStrings(credenctial);
+                    }
+                }
+
+                BlipFaceServicesCommunication communication = new BlipFaceServicesCommunication();
+                communication.NotifyUseBlipFace(blipFaceGuid, version.ToString());
+            }catch(Exception ex)
+            {
+                string mes = ex.Message;
+                if(ex.InnerException!=null)
+                {
+                    mes += Environment.NewLine + "Inner exp: " + ex.InnerException.Message;
+                }
+                System.Windows.Forms.MessageBox.Show(mes);
+            }
         }
     }
 }
